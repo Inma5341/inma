4395e48373a4258da6b8898fdbc60f79a8eb0fa0
inma <24ai-inma5341@yit.edu.in>
2025-10-27 14:13:21 +0530

    Add files via upload

---
+++ lab 16 a.R
@@ -0,0 +1,11 @@
+double_var<-3.14
+integer_var<-42L
+logical_var<-TRUE
+complex_var<-1 + 2i
+character_var<- "Hello,World!"
+
+cat("double variable:",double_var,"-type:",typeof(double_var),"\n")
+cat("integer variable:",integer_var,"-type:",typeof(integer_var),"\n")
+cat("logical variable:",logical_var,"-type:",typeof(logical_var),"\n")
+cat("complex variable:",complex_var,"-type:",typeof(complex_var),"\n")
+cat("character variable:",character_var,"-type:",typeof(character_var),"\n")
